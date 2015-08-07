c94e9101c141c3d43a49b258cf9bc47243fc5d91
Qerts <michal.balicky@gmail.com>
2015-08-07 16:06:45 +0200

    Smazán ORM

---
+++ StoryHub/StoryHubWCFApp/MyRESTService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace StoryHubWCFApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyRESTService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyRESTService.svc or MyRESTService.svc.cs at the Solution Explorer and start debugging.
+    public class MyRESTService : IMyRESTService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
